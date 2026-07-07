c941a9e9e3d316f79fd03543aff214c026fabd8f
pernou_e <pernou_e@etna-alternance.net>
2026-07-07 16:12:35 +0200

    fix: update Document model to use JSON type for content field

--- word_project/server/app/models.py
+++ word_project/server/app/models.py
@@ -1,7 +1,8 @@
 """ app/models.py """
 
 
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, JSON
+from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
 
@@ -21,7 +22,7 @@ class Document(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False, default="Document sans titre")
-    content = Column(Text, nullable=True)  # HTML ou JSON
+    content = Column(JSON, nullable=True)  # HTML ou JSON
     owner_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
